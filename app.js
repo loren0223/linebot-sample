c2571c09301ae0a6a16d9896f9defb124ed5b338
Loren Cheng <loren0223@gmail.com>
2020-04-10 00:18:42 +0800

    using single quote

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const line = require("@line/bot-sdk");
-const express = require("express");
-const lineRouter = require("./routes/line");
+const fs = require('fs');
+const path = require('path');
+const line = require('@line/bot-sdk');
+const express = require('express');
+const lineRouter = require('./routes/line');
 
 // create Express app
 const app = express();
-
+//
 // mount linebot middleware
-app.use("/line/callback", lineRouter);
+app.use('/line/callback', lineRouter);
 
 // listen on port
 const port = process.env.PORT || 3000;
